24da775f472c34370e8f88ee12c2311495afae28
Pietro Dorighi <36574756+piedor@users.noreply.github.com>
2019-04-12 11:24:05 +0200

    suggesto_instagram_web_api.js first version

---
+++ demo_instagram_web_api/suggesto_instagram_web_api.js
@@ -0,0 +1,52 @@
+var respJson;
+
+function getJson(url){
+    respJson = '';
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success: function(data, status, xhr)
+        {
+            respJson = data;
+        }        
+    });
+}
+
+function getMediaByHashtag(hashtag){
+    getJson('https://www.instagram.com/explore/tags/' + hashtag + '/?__a=1');
+    var listaUrl = [];
+    for(var i in respJson["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"]){
+        listaUrl.push(respJson["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"][i]["node"]["thumbnail_src"]);
+    }
+    return(listaUrl);
+}
+
+// prova a cercare val di fiemme (id=219073202)
+function getMediaByLocationId(id){
+    getJson('https://www.instagram.com/explore/locations/' + id + '/?__a=1');
+    var listaUrl = [];
+    for(var i in respJson["graphql"]["location"]["edge_location_to_media"]["edges"]){
+        listaUrl.push(respJson["graphql"]["location"]["edge_location_to_media"]["edges"][i]["node"]["thumbnail_src"]);
+    }
+    return(listaUrl);
+}
+
+function getMediaByUsername(username){
+    // Puoi vedere il profilo degli account pubblici e il tuo
+    getJson('https://www.instagram.com/' + username + '/?__a=1');
+    return(respJson);
+}
+
+function getMediaByAccountId(id){
+    // Puoi vedere il profilo degli account pubblici e il tuo
+    getJson('https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"' + id + '","first":12,"after":""}');
+    return(respJson);
+}
+
+function getInfoAccountId(id){
+    getJson('https://i.instagram.com/api/v1/users/' + id + '/info/');
+    return(respJson);
+}
+
